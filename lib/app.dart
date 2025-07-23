c6719381bd01a6d5c26a780dbe465254f7dcead9
Victoria Caixeta <victoriarabelocaixeta28@gmail.com>
2025-07-23 11:25:04 -0300

    exercicio 1

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 // import 'presentation/pages/stack/home_page3.dart';
 //import 'presentation/pages/text/horaDePraticar1/Atividade1.dart';
 // import 'presentation/pages/text/horaDePraticar1/Atividade2.dart';
-import 'presentation/pages/text/horaDePraticar1/Atividade3.dart';
+//import 'presentation/pages/text/horaDePraticar1/Atividade3.dart';
+import 'presentation/pages/container/home_page3.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
